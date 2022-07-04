0b5f9bf8079ea4d2f53a16522ea06c30baeea23f
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-04 16:10:59 -0700

    101 quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,25 +5,13 @@
 #include <unistd.h>
 
 /**
- *main - main block
- *
- *Return: Always 1 (Success)
+ *main - prints exactly "and that place of art is useful"
+ *"- Dora Korpar, 2015-10-19",
+ *followed by new line, to the standard error.
+ *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-		int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-
-
-			write(2,
-
-						      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-
-						      	      s);
-
-				return (1);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+return (1);
 }
