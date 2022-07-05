c9ab4a470ee1314b8cd871669448c98ffbed9d22
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-05 12:05:24 -0700

    o-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,11 @@
 
 /**
  *
- *  * main - Print "_putchar" followed by a new line.
+ *main - Print "_putchar" followed by a new line.
  *
- *   * Description: You are not allowed to include standard libraries.
+ *Return: 0
  *
- *    * Return: 0
- *
- *     */
+ */
 
 int main(void)
 {
