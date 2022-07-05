2224ebcdf0ab7645609f571881743ca1b3405597
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-05 13:49:13 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,15 +7,12 @@
  */
 int _abs(int n)
 {
-		if (n < 0)
-				{
-
-							return (n * -1);
-								}
-			else
-					{
-
-								return (n);
-
-									}
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
 }
