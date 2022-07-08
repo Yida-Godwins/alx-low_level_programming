9d309524a15e155d888daa2edf48cd3125e0c58c
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-07 20:40:53 -0700

    0main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,32 +1,10 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 #include "main.h"
-
-int main(void)
-{
-int n;
-srand(time(0));
-n = 0;
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is positive\n", n);
-}
-return (0);
-}
 
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 int i;
