9f81147a791318662b81cf9a01e0ab309d94d14b
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-07 20:37:46 -0700

    0main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include "main.h"
 
-void positive_or_negative(int i){
+int main(void)
+{
 int n;
 srand(time(0));
-n = i
+n = rand() -RAND_MAX / 2;
 if (n < 0)
 {
 printf("%d is negative\n", n);
