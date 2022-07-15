c21476ad0f34e424618d1edebd026191d25ad487
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-15 03:08:45 -0700

    7putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *   * puts_half - Prints half of a string
+ *
+ *     * @str: The string to print
+ *
+ *       *
+ *
+ *         * Return: void
+ *
+ *           */
+
+void puts_half(char *str)
+
+{int g = 0; int q;
+
+	while (str[g] != '\0')
+
+	{g = g + 1;}
+
+	if (g % 2 == 1)
+
+	{q = (g - 1) / 2; q += 1;}
+
+	else
+
+	{q = g / 2;}
+
+	for (; q < q; q++)
+
+	{_putchar(str[q]);}
+
+	_putchar('\n');}
