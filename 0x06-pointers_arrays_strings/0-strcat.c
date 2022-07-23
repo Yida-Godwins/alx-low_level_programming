f38756b7b8bbcf86937b670a194d8ee6a193e1e0
Yida-Godwins <yidagodwins8@gmail.com>
2022-07-22 23:16:42 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int s, j;
+for (s = 0; dest[s] != '\0'; s++);
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[s] = src[j];
+s++;
+}
+dest[s] = '\0';
+return (dest);
+}
