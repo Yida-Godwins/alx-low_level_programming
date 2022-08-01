a733476f18e6fff4490afb00be7c8c349af373c1
Yida-Godwins <yidagodwins8@gmail.com>
2022-08-01 14:24:31 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,6 @@ float age;
 char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
