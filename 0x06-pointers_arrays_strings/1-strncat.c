8cb0e221b8fac2365d8be8f28a71f26c9c4066c4
Yida-Godwins <yidagodwins8@gmail.com>
2022-08-12 15:09:29 -0700

    1strnct

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,6 +10,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-strncat(dest, *src, int n);
+int dlen = 0, j = 0;
+while (dest[dlen])
+{
+dlen++;
+}
+while (j < n && src[j])
+{
+dest[dlen] = src[j];
+dlen++;
+j++;
+}
+dest[dlen + n + 1] = '\0';
 return (dest);
+}
 }
